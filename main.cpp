7f03c3123fffcd15852b8d02b9baa19eb908f651
Mattia10 <mattia.di.tecco@gmail.com>
2024-11-09 02:20:02 +0100

    new window camera system and git ignore updated

--- main.cpp
+++ main.cpp
@@ -1,5 +1,49 @@
 #include <SFML/Graphics.hpp>
+#include "Game.h"
+#include "WindowCamera/Camera.h"
 
+int main()
+{
+	sf::RenderWindow window(sf::VideoMode(1920, 1080), "Mario Game");
+	sf::Clock deltaClock;
+	Camera camera;
+
+
+	Begin(window);
+	while (window.isOpen())
+	{
+		float deltaTime = deltaClock.restart().asSeconds();
+
+		sf::Event event{};
+		while (window.pollEvent(event))
+		{
+			if (event.type == sf::Event::Closed)
+				window.close();
+		}
+		window.setView(camera.GetView(window.getSize()));
+		Update(deltaTime);
+
+		window.clear();
+
+		// draw here
+		Render(window);
+
+		window.display();
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+/*
 int main()
 {
 	sf::RenderWindow window(sf::VideoMode(200, 200), "Mario Test");
@@ -22,3 +66,4 @@ int main()
 
 	return 0;
 }
+*/
